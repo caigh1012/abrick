65977ce63ad2bd4c68ed37caeeeca5cb66b86a44
caigh1012 <19925338175@163.com>
2024-07-03 22:05:00 +0800

    feat: initialize

---
+++ types/index.cjs.d.ts
@@ -0,0 +1,10 @@
+declare function isNumber(n: number): boolean;
+
+declare function isString(str: string): boolean;
+
+declare const _default: {
+    isNumber: typeof isNumber;
+    isString: typeof isString;
+};
+
+export { _default as default };
